18a17a7812d7f40f90ff7114ad4fe81e531e22c9
soumyabrata1234 <raysinha1729@gmail.com>
2025-06-19 15:37:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.get('/temp',(req,res)=>{
+  res.send("hii");
+})
 // Routes
 app.use('/user', userroutes);
 app.use('/project', projectroutes);
