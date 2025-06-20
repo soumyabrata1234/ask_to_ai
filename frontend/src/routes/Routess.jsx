60e91b670f9176ccf10006db59db682dadd29d04
soumyabrata1234 <raysinha1729@gmail.com>
2025-06-20 10:49:46 +0530

    Update Routess.jsx

--- frontend/src/routes/Routess.jsx
+++ frontend/src/routes/Routess.jsx
@@ -1,20 +1,19 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../screens/Home.jsx';
 import Login from '../screens/Login.jsx';
 import Signup from '../screens/Signup.jsx';
 import UserAuth from '../screens/userauth.jsx';
 import Project from '../screens/project.jsx';
 import Extraform from '../screens/ExtraForm.jsx';
+
 export default function Routess() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<UserAuth><Home/></UserAuth>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/project" element={<Project/>} />
-        <Route path='/xyz' element={<Extraform />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<UserAuth><Home /></UserAuth>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/xyz" element={<Extraform />} />
+    </Routes>
   );
 }
